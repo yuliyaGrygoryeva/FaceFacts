36986bf77bb82ed22baca4bd1c7d8e74f59b5e1e
Yuliya Grygoryeva <yuliyagrygoryeva@Yuliyas-MacBook-Pro.local>
2024-01-06 20:34:43 -0800

    Empty project

---
+++ FaceFacts/FaceFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceFacts
+//
+//  Created by Yuliya Grygoryeva on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceFacts/FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Yuliya Grygoryeva on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
