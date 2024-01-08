84500c6c18dacd7283c45c485866a827cb847ee4
Yuliya Grygoryeva <yuliyagrygoryeva@Yuliyas-MacBook-Pro.local>
2024-01-07 16:46:45 -0800

    Add photos selector

--- FaceFacts/FaceFacts/EditPersonView.swift
+++ FaceFacts/FaceFacts/EditPersonView.swift
@@ -5,6 +5,7 @@
 //  Created by Yuliya Grygoryeva on 1/7/24.
 //
 
+import PhotosUI
 import SwiftData
 import SwiftUI
 
@@ -12,6 +13,7 @@ struct EditPersonView: View {
     @Environment(\.modelContext) var modelContext
     @Bindable var person: Person
     @Binding var navigationPath: NavigationPath
+    @State private var selectedItem: PhotosPickerItem?
     
     @Query(sort: [
         SortDescriptor(\Event.name),
@@ -20,6 +22,17 @@ struct EditPersonView: View {
     
     var body: some View {
         Form {
+            Section {
+                if let imageData = person.photo, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                }
+                PhotosPicker(selection: $selectedItem, matching: .images) {
+                    Label("Select a photo", systemImage: "person")
+                }
+            }
+            
             Section {
                 TextField("Name", text: $person.name)
                     .textContentType(.name)
@@ -54,12 +67,19 @@ struct EditPersonView: View {
         .navigationDestination(for: Event.self) { event in
             EditEventView(event: event)
         }
+        .onChange(of: selectedItem, loadPhoto)
     }
     
     func addEvent() {
         let event = Event(name: "", location: "")
         modelContext.insert(event)
         navigationPath.append(event)
+    }
+    
+    func loadPhoto() {
+        Task { @MainActor in
+            person.photo = try await selectedItem?.loadTransferable(type: Data.self)
+        }
     }
 }
 
